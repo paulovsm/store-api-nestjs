d2b0f023cd4b555c093b8a90d5997cb62966e6bb
paulovsm <paulovsm@gmail.com>
2023-07-20 19:17:41 +0000

    working in progress
    # Aider chat conversation:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ProdutosModule } from './produtos/produtos.module';
 import { UsuariosModules } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [UsuariosModules, ProdutosModule]
+  imports: [
+    UsuariosModules,
+    ProdutosModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+  
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
