b024b473aff36aa090973062d5ba14132f8bcdd2
paulovsm <paulovsm@gmail.com>
2023-07-20 19:43:19 +0000

    aider: Added @Entity annotation above the Produto interface, added @PrimaryGeneratedColumn annotation above the id property, and added @Column annotation above the usuarioId property.
    # Aider chat conversation:
    
    USER: Como trabalharemos com a @ProdutoEntity, escreveremos na linha acima dela a anotação @Entity({ name: 'produtos'}). Por que usamos o nome "produtos" se a tabela nem existe ainda? Porque já sabemos que, quando o nome da classe é "Produto" ou "Usuario" no singular, o nome da tabela geralmente será o nome da entidade no plural ("Produtos" e "Usuarios", respectivamente), conforme as boas práticas adotadas.
    
    Pressionaremos a tecla "Enter" loco acima do id e escreveremos a anotação @PrimaryGeneratedColumn('uuid'). Este é um conceito interessante de ser usado para evitar colisão.
    
    Continuando, pressionaremos "Enter" logo acima de usuarioId para escrever a anotação @Column({ name: 'usuario_id'}), seguindo a mesma prática que usamos para nomear o createdAt, o updatedAt e o deletedAt no vídeo anterior.
    
    Como length, podemos acrescentar 100 e ele também não pode ser nulo (nullable: false)
    
    ASSISTANT: Para fazer as alterações que você solicitou, preciso que você forneça acesso de gravação ao arquivo `src/models/produto.entity.ts`. Por favor, altere as permissões deste arquivo para leitura e gravação e me avise quando estiver pronto.
    
    USER: I added these *read-write* files: src/models/produto.entity.ts
    
    ASSISTANT: Great! Now we can proceed with the changes. Here are the steps:
    
    1. Add the `@Entity({ name: 'produtos'})` annotation above the `Produto` interface.
    2. Add the `@PrimaryGeneratedColumn('uuid')` annotation above the `id` property.
    3. Add the `@Column({ name: 'usuario_id', length: 100, nullable: false })` annotation above the `usuarioId` property.
    
    Here are the changes in the `edit block` format:
    
    ```typescript
    src/models/produto.entity.ts
    <<<<<<< ORIGINAL
    import { CaracteristicaProduto } from "./caracteristicaProduto"
    import { ImagemProduto } from "./imagemProduto"
    
    export interface Produto {
        id: string,
        nome: string,
        valor: number,
        quantidadeDisponivel: number,
        descricao: string,
        caracteristicas: CaracteristicaProduto[],
        imagens: ImagemProduto[],
        categoria: string,
        dataCriacao: Date,
        dataAtualizacao: Date,
        usuarioId: string
    }
    =======
    import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
    import { CaracteristicaProduto } from "./caracteristicaProduto"
    import { ImagemProduto } from "./imagemProduto"
    
    @Entity({ name: 'produtos'})
    export class Produto {
        @PrimaryGeneratedColumn('uuid')
        id: string;
    
        nome: string;
        valor: number;
        quantidadeDisponivel: number;
        descricao: string;
        caracteristicas: CaracteristicaProduto[];
        imagens: ImagemProduto[];
        categoria: string;
        dataCriacao: Date;
        dataAtualizacao: Date;
    
        @Column({ name: 'usuario_id', length: 100, nullable: false })
        usuarioId: string;
    }
    >>>>>>> UPDATED
    ```
    
    Please review the changes and let me know if they are correct.

--- src/models/produto.entity.ts
+++ src/models/produto.entity.ts
@@ -1,16 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 import { CaracteristicaProduto } from "./caracteristicaProduto"
 import { ImagemProduto } from "./imagemProduto"
 
-export interface Produto {
-    id: string,
-    nome: string,
-    valor: number,
-    quantidadeDisponivel: number,
-    descricao: string,
-    caracteristicas: CaracteristicaProduto[],
-    imagens: ImagemProduto[],
-    categoria: string,
-    dataCriacao: Date,
-    dataAtualizacao: Date,
-    usuarioId: string
+@Entity({ name: 'produtos'})
+export class Produto {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    nome: string;
+    valor: number;
+    quantidadeDisponivel: number;
+    descricao: string;
+    caracteristicas: CaracteristicaProduto[];
+    imagens: ImagemProduto[];
+    categoria: string;
+    dataCriacao: Date;
+    dataAtualizacao: Date;
+
+    @Column({ name: 'usuario_id', length: 100, nullable: false })
+    usuarioId: string;
 }
