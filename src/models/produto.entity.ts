3fe41bedced1033dee5c9184c5591ea15edb4c48
paulovsm <paulovsm@gmail.com>
2023-07-20 19:44:13 +0000

    aider: Added annotations to the properties `nome`, `valor`, `quantidadeDisponivel`, `descricao`, `categoria`, `dataCriacao`, `dataAtualizacao` in `src/models/produto.entity.ts`, removed `length` for `valor` and `quantidadeDisponivel`, and set `length` to 255 for `descricao`.
    # Aider chat conversation:
    
    USER: Com essa anotação feita, podemos copiar toda a linha e colá-la sobre cada linha seguinte, renomeando o nome de cada item. No caso de valor e quantidade, suprimiremos o length por questões específicas deste cenário. Não entraremos em detalhes agora, mas podemos abordar o tema posteriormente.
    
    Em descricao, usaremos o length em 255, pois esse número é suficiente para uma boa descrição. Terminada essa edição, ainda temos os campos de caracteristicas e imagens.
    
    ASSISTANT: Para fazer as alterações que você solicitou, precisamos modificar o arquivo `src/models/produto.entity.ts`. Aqui estão as etapas:
    
    1. Adicione a anotação `@Column()` acima das propriedades `nome`, `valor`, `quantidadeDisponivel`, `descricao`, `categoria`, `dataCriacao`, `dataAtualizacao`.
    2. Para `descricao`, defina o `length` como `255`.
    3. Remova o `length` para `valor` e `quantidadeDisponivel`.
    
    Aqui estão as mudanças no formato de `edit block`:
    
    ```typescript
    src/models/produto.entity.ts
    <<<<<<< ORIGINAL
    @Entity({ name: 'produtos'})
    export class Produto {
        @PrimaryGeneratedColumn('uuid')
        id: string;
    
        nome: string;
        valor: number;
        quantidadeDisponivel: number;
        descricao: string;
        caracteristicas: CaracteristicaProduto[];
        imagens: ImagemProduto[];
        categoria: string;
        dataCriacao: Date;
        dataAtualizacao: Date;
    
        @Column({ name: 'usuario_id', length: 100, nullable: false })
        usuarioId: string;
    }
    =======
    @Entity({ name: 'produtos'})
    export class Produto {
        @PrimaryGeneratedColumn('uuid')
        id: string;
    
        @Column({ nullable: false })
        nome: string;
    
        @Column({ nullable: false })
        valor: number;
    
        @Column({ nullable: false })
        quantidadeDisponivel: number;
    
        @Column({ length: 255, nullable: false })
        descricao: string;
    
        caracteristicas: CaracteristicaProduto[];
        imagens: ImagemProduto[];
    
        @Column({ nullable: false })
        categoria: string;
    
        @Column({ nullable: false })
        dataCriacao: Date;
    
        @Column({ nullable: false })
        dataAtualizacao: Date;
    
        @Column({ name: 'usuario_id', length: 100, nullable: false })
        usuarioId: string;
    }
    >>>>>>> UPDATED
    ```
    
    Por favor, revise as mudanças e me avise se estão corretas.

--- src/models/produto.entity.ts
+++ src/models/produto.entity.ts
@@ -7,14 +7,28 @@ export class Produto {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column({ nullable: false })
     nome: string;
+
+    @Column({ nullable: false })
     valor: number;
+
+    @Column({ nullable: false })
     quantidadeDisponivel: number;
+
+    @Column({ length: 255, nullable: false })
     descricao: string;
+
     caracteristicas: CaracteristicaProduto[];
     imagens: ImagemProduto[];
+
+    @Column({ nullable: false })
     categoria: string;
+
+    @Column({ nullable: false })
     dataCriacao: Date;
+
+    @Column({ nullable: false })
     dataAtualizacao: Date;
 
     @Column({ name: 'usuario_id', length: 100, nullable: false })
