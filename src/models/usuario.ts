c53b57f9835334bd8bec9b825f03a0795c6269c2
paulovsm <paulovsm@gmail.com>
2023-07-20 19:19:43 +0000

    aider: Added TypeORM annotations to the UsuarioEntity class.
    # Aider chat conversation:
    
    USER: Inseriremos algumas funções da Biblioteca do TypeORM para enriquecer a entidade. Primeiro, abriremos uma nova linha no começo do arquivo pressionando a tecla "Enter" e nela escreveremos import {Entity, Column} from 'typeorm'. Com isso, importaremos os módulos Entity e Column. À medida que o projeto evoluir, importaremos outros módulos.
    
    Em seguida, anotaremos a classe UsuarioEntity com a anotação @Entity({}) logo acima do nome da classe. Dentro das chaves, definiremos um name, que corresponde ao nome que está no banco de dados.
    
    Lembrando que a tabela no banco de dados ainda não existe, mas já temos uma noção de que, como a entidade se chama "Usuário" (UsuarioEntity), a boa prática é nomear o banco de dados com o nome designado no plural, ou seja, "Usuários". Assim, a anotação completa será @Entity({ name: 'usuarios' }).
    
    Por enquanto, não estamos indicando ao TypeORM que as linhas contidas na classe se referem a campos de uma tabela. Para fazer isso, começaremos editando o nome. Ele será anotado com @Column({}), que também conterá um name.
    
    Neste caso, para seguirmos as recomendações, o nome será o mesmo: 'nome'. Com isso, definimos que a nossa tabela com dados de usuários precisa ter uma coluna chamada "nome".
    
    Acontece que conseguimos inserir outras configurações na coluna, como o length. Para o exemplo do nome, pensemos que ele normalmente não terá mais que 100 caracteres.
    
    Esse limite pode ser inserido na coluna. Dessa forma, caso alguém digite um nome com mais de 100 caracteres, surgirá na tela uma mensagem de erro.
    
    Isso é interessante porque o banco de dados será otimizado para o que precisamos em vez de ter campos com tamanho aleatório. Por enquanto, definiremos o tamanho da coluna em 100. Por fim, podemos inserir um nullable falso. Isso significa que o campo não pode ser nullable, o famoso "not null" do banco de dados.
    
    ASSISTANT: To make these changes, I need you to make the file `src/models/usuario.ts` *read-write*.
    
    Once you've done that, I can provide the specific changes to the code.
    
    USER: I added these *read-write* files: src/models/usuario.ts
    
    ASSISTANT: Great! Now we can proceed with the changes.
    
    Here are the steps:
    
    1. Import the `Entity` and `Column` modules from 'typeorm' at the top of the file.
    2. Annotate the `Usuario` interface with `@Entity({ name: 'usuarios' })`.
    3. Annotate each field with `@Column({})` and add the necessary configurations.
    
    Here are the changes in the `src/models/usuario.ts` file:
    
    ```typescript
    src/models/usuario.ts
    <<<<<<< ORIGINAL
    export interface Usuario {
        id: string;
        nome: string;
        email: string;
        senha: string;
    }
    =======
    import { Entity, Column } from 'typeorm';
    
    @Entity({ name: 'usuarios' })
    export class Usuario {
        @Column({ type: 'varchar', length: 36 })
        id: string;
    
        @Column({ type: 'varchar', length: 100, nullable: false })
        nome: string;
    
        @Column({ type: 'varchar', length: 100, nullable: false })
        email: string;
    
        @Column({ type: 'varchar', length: 100, nullable: false })
        senha: string;
    }
    >>>>>>> UPDATED
    ```
    
    Please note that I've changed `Usuario` from an interface to a class because decorators like `@Entity` and `@Column` can only be used with classes, not interfaces. Also, I've assumed that the `id`, `email`, and `senha` fields should also be non-nullable and have a maximum length of 100 characters. If these assumptions are incorrect, please let me know so I can adjust the code accordingly.

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -1,6 +1,16 @@
-export interface Usuario {
+import { Entity, Column } from 'typeorm';
+
+@Entity({ name: 'usuarios' })
+export class Usuario {
+    @Column({ type: 'varchar', length: 36 })
     id: string;
+
+    @Column({ type: 'varchar', length: 100, nullable: false })
     nome: string;
+
+    @Column({ type: 'varchar', length: 100, nullable: false })
     email: string;
+
+    @Column({ type: 'varchar', length: 100, nullable: false })
     senha: string;
 }
