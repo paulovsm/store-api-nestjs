669c0a3624f49f546d60135293cd71376a34f0b6
paulovsm <paulovsm@gmail.com>
2023-07-25 20:33:37 +0000

    aider: Implement create, findAll, findOne, update, and remove methods in pedido.service.ts using produto.service.ts as a reference.
    # Aider chat conversation:
    
    USER: Update pedido.service.ts using produto.service.ts as reference in order to implement all dummy methods (create, findAll, findOne, update, remove)
    
    ASSISTANT: To implement the methods in `pedido.service.ts` using `produto.service.ts` as a reference, we will need to modify the `pedido.service.ts` file.
    
    Here are the steps we will follow:
    
    1. Import necessary dependencies and entities.
    2. Implement the `create` method.
    3. Implement the `findAll` method.
    4. Implement the `findOne` method.
    5. Implement the `update` method.
    6. Implement the `remove` method.
    
    Let's start with the first step:
    
    ```typescript
    src/pedido/pedido.service.ts
    <<<<<<< ORIGINAL
    import { Injectable } from '@nestjs/common';
    import { CreatePedidoDto } from './dto/create-pedido.dto';
    import { UpdatePedidoDto } from './dto/update-pedido.dto';
    import { InjectRepository } from '@nestjs/typeorm';
    import { Pedido } from './pedido.entity';
    import { Usuario } from 'src/models/usuario.entity';
    import { Repository } from 'typeorm';
    =======
    import { Injectable, Inject } from '@nestjs/common';
    import { CreatePedidoDto } from './dto/create-pedido.dto';
    import { UpdatePedidoDto } from './dto/update-pedido.dto';
    import { InjectRepository } from '@nestjs/typeorm';
    import { Pedido } from './pedido.entity';
    import { Usuario } from 'src/models/usuario.entity';
    import { Repository } from 'typeorm';
    import { v4 as uuidv4 } from 'uuid';
    >>>>>>> UPDATED
    ```
    
    Next, we will implement the `create` method:
    
    ```typescript
    src/pedido/pedido.service.ts
    <<<<<<< ORIGINAL
      create(createPedidoDto: CreatePedidoDto) {
        return 'This action adds a new pedido';
      }
    =======
      async create(createPedidoDto: CreatePedidoDto) {
        const pedido = new Pedido();
        Object.assign(pedido, createPedidoDto);
        const pedidoCriado = await this.pedidoRepository.save(pedido);
    
        if(!pedidoCriado) {
            throw new Error('Erro ao criar pedido');
        }
    
        return pedidoCriado.id;
      }
    >>>>>>> UPDATED
    ```
    
    Now, let's implement the `findAll` method:
    
    ```typescript
    src/pedido/pedido.service.ts
    <<<<<<< ORIGINAL
      findAll() {
        return `This action returns all pedido`;
      }
    =======
      async findAll(): Promise<Pedido[]> {
        return await this.pedidoRepository.find();
      }
    >>>>>>> UPDATED
    ```
    
    Next, we will implement the `findOne` method:
    
    ```typescript
    src/pedido/pedido.service.ts
    <<<<<<< ORIGINAL
      findOne(id: number) {
        return `This action returns a #${id} pedido`;
      }
    =======
      async findOne(id: string): Promise<Pedido> {
        const pedido = await this.pedidoRepository.findOne({ id });
    
        if (!pedido) {
            throw new Error('Pedido não encontrado');
        }
    
        return pedido;
      }
    >>>>>>> UPDATED
    ```
    
    Now, let's implement the `update` method:
    
    ```typescript
    src/pedido/pedido.service.ts
    <<<<<<< ORIGINAL
      update(id: number, updatePedidoDto: UpdatePedidoDto) {
        return `This action updates a #${id} pedido`;
      }
    =======
      async update(id: string, updatePedidoDto: UpdatePedidoDto): Promise<Pedido> {
        const pedidoExistente = await this.findOne(id);
    
        Object.assign(pedidoExistente, updatePedidoDto);
    
        await this.pedidoRepository.save(pedidoExistente);
    
        return pedidoExistente;
      }
    >>>>>>> UPDATED
    ```
    
    Finally, we will implement the `remove` method:
    
    ```typescript
    src/pedido/pedido.service.ts
    <<<<<<< ORIGINAL
      remove(id: number) {
        return `This action removes a #${id} pedido`;
      }
    =======
      async remove(id: string): Promise<Pedido> {
        const pedidoExistente = await this.findOne(id);
    
        await this.pedidoRepository.delete(id);
    
        return pedidoExistente;
      }
    >>>>>>> UPDATED
    ```
    
    Please review these changes and let me know if they meet your requirements.

--- src/pedido/pedido.service.ts
+++ src/pedido/pedido.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Pedido } from './pedido.entity';
 import { Usuario } from 'src/models/usuario.entity';
 import { Repository } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class PedidoService {
@@ -15,23 +16,47 @@ export class PedidoService {
     private readonly usuarioRepository: Repository<Usuario>,
   ) { }
 
-  create(createPedidoDto: CreatePedidoDto) {
-    return 'This action adds a new pedido';
+  async create(createPedidoDto: CreatePedidoDto) {
+    const pedido = new Pedido();
+    Object.assign(pedido, createPedidoDto);
+    const pedidoCriado = await this.pedidoRepository.save(pedido);
+
+    if(!pedidoCriado) {
+        throw new Error('Erro ao criar pedido');
+    }
+
+    return pedidoCriado.id;
   }
 
-  findAll() {
-    return `This action returns all pedido`;
+  async findAll(): Promise<Pedido[]> {
+    return await this.pedidoRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pedido`;
+  async findOne(id: string): Promise<Pedido> {
+    const pedido = await this.pedidoRepository.findOne({ id });
+
+    if (!pedido) {
+        throw new Error('Pedido não encontrado');
+    }
+
+    return pedido;
   }
 
-  update(id: number, updatePedidoDto: UpdatePedidoDto) {
-    return `This action updates a #${id} pedido`;
+  async update(id: string, updatePedidoDto: UpdatePedidoDto): Promise<Pedido> {
+    const pedidoExistente = await this.findOne(id);
+
+    Object.assign(pedidoExistente, updatePedidoDto);
+
+    await this.pedidoRepository.save(pedidoExistente);
+
+    return pedidoExistente;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pedido`;
+  async remove(id: string): Promise<Pedido> {
+    const pedidoExistente = await this.findOne(id);
+
+    await this.pedidoRepository.delete(id);
+
+    return pedidoExistente;
   }
 }
